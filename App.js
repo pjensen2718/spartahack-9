c638d08e71fcc682e8397b862f9a441bc07d5c11
ericYuan17 <ericyuan@umich.edu>
2024-01-27 18:37:28 -0500

    experimental; dashboard can now be opened through App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from "./pages/dashboard.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <Dashboard/>
   );
 }
 
