522e884fc34b57f98b7fc07f99eb0ce74aa7f4e0
Maui <maui.baba1@gmail.com>
2024-01-27 18:43:54 -0500

    i really want to get hit by a truck why am i here idek hwo to use react native ive never even done app dev why why why why wyh

---
+++ pages/profile.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Button, StyleSheet, Text, View } from "react-native";
+// import firestore from '@react-native-firebase/firestore';
+
+const ProfileScreen = () => {
+    return {
+         
+    };
+};
