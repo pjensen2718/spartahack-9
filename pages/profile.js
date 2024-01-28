9553f2c2debc76b83b726039d598c505d1732d62
ericYuan17 <ericyuan@umich.edu>
2024-01-28 05:48:59 -0500

    Editable account security fields,rudimentary boxes

--- pages/profile.js
+++ pages/profile.js
@@ -1,25 +1,37 @@
-import React from "react";
-import {Button, StyleSheet, Text, View, ImageBackground, ScrollView, Dimensions} from "react-native";
+import React, { useState }  from "react";
+import {Button, StyleSheet, Text, View, ImageBackground, ScrollView, Dimensions, TextInput} from "react-native";
 import { Card, CardFooter } from '../components/card';
 import { GlobalText, GlobalColor } from '../components/global_styles';
 // import firestore from '@react-native-firebase/firestore';
 
 export default function ProfileScreen() {
     console.log("-> Profile")
+
+    const [email, setEmail] = useState('example@email.com');
+    const [name, setName] = useState('Leeroy Jenkins');
+    const [password, setPassword] = useState('Password123');
+
     return (
         <ScrollView style={GlobalColor.bg}>
             <Card>
                 <Text style = {GlobalText.headerText}>
                     Account Security
                 </Text>
-                <Text style = {GlobalText.normalText}>
-                    {"\n"}
-                    name
-                    {"\n"}{"\n"}
-                    email
-                    {"\n"}{"\n"}
-                    password
-                </Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setName}
+                    value={name}
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setEmail}
+                    value={email}
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setPassword}
+                    value={password}
+                />
             </Card>
 
             <Card>
@@ -57,4 +69,14 @@ export default function ProfileScreen() {
 
         </ScrollView>
     );
+
+    
 }
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
