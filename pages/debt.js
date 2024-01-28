a597177d4b4be96642102cbf4e452f5bfddfa7ac
AtlasLVI <maxweiten@outlook.com>
2024-01-28 00:44:36 -0500

    Debts fixed

--- pages/debt.js
+++ pages/debt.js
@@ -35,14 +35,3 @@ export default function Debt() {
         </View>
     );
 }
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
